632a2ecb29cbaf4a8af52b42b94aa36596ad03f2
polllkikh <106408382+polllkikh@users.noreply.github.com>
2022-06-03 15:17:56 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+__author__ = 'Poliana'
+
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+s = n1 + n2
+
+print('A soma do numero {} e o numero é igual {}'.format(n1, n2, s))
